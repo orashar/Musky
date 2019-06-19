dbbcc2113eeee4c217192e18c6c96ae2342c541b
OM PARASHAR <omparashar15@gmail.com>
2019-06-19 18:42:13 +0530

    Update client.py

--- client.py
+++ client.py
@@ -25,7 +25,7 @@ def send():
 
 def display(data):
     for message in data:
-        l = Label(root, text=message).pack(side=TOP)
+        l = Label(root, text=message, bg="#aeee8c").grid(row=0, column=0, columnspan=2, sticky="NSEW")
         data.remove(message)
 
 
@@ -40,8 +40,8 @@ skt.connect((host, port))
 start_new_thread(receive, (host, port))
 
 smsg = StringVar()
-entry = Entry(root, textvariable=smsg).pack(side=BOTTOM, fill=X)
+entry = Entry(root, textvariable=smsg).grid(row=1, column=0, sticky="NSEW")
 
-sendb = Button(root, text='send', command=send).pack(side=BOTTOM)
+sendb = Button(root, text='     send     ', command=send, bg="#93ff5b").grid(row=1, column=1, sticky="NSEW")
 
 mainloop()
